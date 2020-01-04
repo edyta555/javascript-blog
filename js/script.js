fb24e0a346a8a1e7891fe459569fbc44125f5e51
Edyta Badowska <edyta.badowska555@gmail.com>
2020-01-04 04:36:45 +0100

    Add js for removing active class

--- js/script.js
+++ js/script.js
@@ -9,10 +9,20 @@ const titleClickHandler = function(event){
   console.log(event);
 
   /* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles) {
+  	activeArticle.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
