1aaa6020a0a3dae56674ff503f4b98888f23349b
Edyta Badowska <edyta.badowska555@gmail.com>
2020-01-26 00:44:50 +0100

    Add generating of title links

--- js/script.js
+++ js/script.js
@@ -1,42 +1,79 @@
-'use strict';
+{
+  'use strict';
+  /*document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+	console.log('links:', links);
+  });*/
+  const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
 
-/*document.getElementById('test-button').addEventListener('click', function(){
-  const links = document.querySelectorAll('.titles a');
-  console.log('links:', links);
-});*/
-const titleClickHandler = function(event){
-	event.preventDefault();
-	const clickedElement = this;
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-	/* [DONE] remove class 'active' from all article links  */
-	const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
 
-	for(let activeLink of activeLinks){
-		activeLink.classList.remove('active');
-	}
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
 
- 	/* [DONE] add class 'active' to the clicked link */
- 	clickedElement.classList.add('active');
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
- 	/* [DONE] remove class 'active' from all articles */
-	const activeArticles = document.querySelectorAll('.posts article.active');
+    for(let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
 
-	for(let activeArticle of activeArticles) {
-		activeArticle.classList.remove('active');
-	}
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
 
-  	/* [DONE] get 'href' attribute from the clicked link */
-	const articleSelector = clickedElement.getAttribute("href");
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
 
-  	/* [DONE] find the correct article using the selector (value of 'href' attribute) */
-	const targetArticle = document.querySelector(articleSelector);
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+  };
 
-  	/* [DONE] add class 'active' to the correct article */
-	targetArticle.classList.add('active');
-}
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
-const links = document.querySelectorAll('.titles a');
+  function generateTitleLinks(){
 
-for(let link of links){
-	link.addEventListener('click', titleClickHandler);
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* find all the articles and save them to variable: articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+    
+    /* for each article */
+    for(let article of articles){
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+
+      /* find the title element and get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+      /* insert link into titleList */
+      html = html + linkHTML;
+      /*another way: titleList.insertAdjacentHTML('beforeend', linkHTML); */
+    }
+
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+	
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+  }
+
+  generateTitleLinks();
 }
